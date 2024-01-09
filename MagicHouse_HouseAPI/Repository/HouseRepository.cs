7d988f925bdfa9d39c2e8fe06f24e348cf206aa4
dominicho97 <dominic.ho97@gmail.com>
2024-01-09 16:31:24 +0100

    Added Repository and implementer IRepository

---
+++ MagicHouse_HouseAPI/Repository/HouseRepository.cs
@@ -0,0 +1,63 @@
+﻿using MagicHouse_HouseAPI.Data;
+using MagicHouse_HouseAPI.Models;
+using MagicHouse_HouseAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MagicHouse_HouseAPI.Repository
+{
+    public class HouseRepository : IHouseRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HouseRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task Create(House entity)
+        {
+            await _db.Houses.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<House> Get(Expression<Func<House, bool>> filter = null, bool tracked = true)
+        {
+            {
+                IQueryable<House> query = _db.Houses;
+                if(!tracked)
+                {
+                    query = query.AsNoTracking();
+                }
+
+
+                if (filter != null)
+                {
+                    query.Where(filter);
+                }
+                return await query.FirstOrDefaultAsync();
+            }
+        }
+
+        public async Task<List<House>> GetAll(Expression<Func<House, bool>> filter = null)
+        {
+            IQueryable<House> query = _db.Houses;
+            if(filter != null)
+            {
+                query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(House entity)
+        {
+             _db.Houses.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ MagicHouse_HouseAPI/Repository/IRepository/IHouseRepository.cs
@@ -0,0 +1,15 @@
+﻿using MagicHouse_HouseAPI.Models;
+using System.Linq.Expressions;
+
+namespace MagicHouse_HouseAPI.Repository.IRepository
+{
+    public interface IHouseRepository
+    {
+        Task<List<House>> GetAll(Expression<Func<House, bool>> filter = null);
+        Task<House>Get(Expression<Func<House, bool>> filter = null, bool tracked=true);
+        Task Create(House entity);
+        Task Remove(House entity);
+
+        Task Save();
+    }
+}
