10f7ebb3dce4d8da0f3caab9b2ac5462a55b3fd6
dominicho97 <dominic.ho97@gmail.com>
2024-01-09 15:00:35 +0100

    Added Async methods to controller

--- MagicHouse_HouseAPI/Controllers/HouseAPIController.cs
+++ MagicHouse_HouseAPI/Controllers/HouseAPIController.cs
@@ -24,10 +24,10 @@ namespace MagicHouse_HouseAPI.Controllers
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<HouseDTO>> GetHouses()
+        public async Task<ActionResult<IEnumerable<HouseDTO>>> GetHouses()
         {
             
-            return Ok(_db.Houses);
+            return Ok(await _db.Houses.ToListAsync());
 
 
         }
@@ -37,13 +37,13 @@ namespace MagicHouse_HouseAPI.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<HouseDTO> GetHouse(int id)
+        public async Task<ActionResult<HouseDTO>> GetHouse(int id)
         {
             if (id == 0)
             {
                 return BadRequest();
             }
-            var house = _db.Houses.FirstOrDefault(u => u.Id == id);
+            var house = await _db.Houses.FirstOrDefaultAsync(u => u.Id == id);
             if (house == null)
             {
                 return NotFound();
@@ -58,14 +58,14 @@ namespace MagicHouse_HouseAPI.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<HouseDTO> CreateHouse([FromBody] HouseCreateDTO houseDTO)
+        public async Task<ActionResult<HouseDTO>> CreateHouse([FromBody] HouseCreateDTO houseDTO)
         {
             //if(!ModelState.IsValid) 
             //{
             //    return BadRequest(ModelState);
             //}
 
-            if (_db.Houses.FirstOrDefault(u => u.Name.ToLower() == houseDTO.Name.ToLower()) != null)
+            if (await _db.Houses.FirstOrDefaultAsync(u => u.Name.ToLower() == houseDTO.Name.ToLower()) != null)
             {
                 ModelState.AddModelError("CustomError", "House already exists!");
                 return BadRequest(ModelState);
@@ -89,8 +89,8 @@ namespace MagicHouse_HouseAPI.Controllers
                 Rate = houseDTO.Rate,
                 Sqft = houseDTO.Sqft,
             };
-            _db.Houses.Add(model);
-            _db.SaveChanges();
+            await _db.Houses.AddAsync(model);
+            await _db.SaveChangesAsync();
 
             return CreatedAtRoute("GetHouse", new { id = model.Id }, model);
         }
@@ -101,19 +101,19 @@ namespace MagicHouse_HouseAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public IActionResult DeleteHouse(int id)
+        public async Task<IActionResult> DeleteHouse(int id)
         {
             if (id == 0)
             {
                 return BadRequest();
             }
-            var house = _db.Houses.FirstOrDefault(u => u.Id == id);
+            var house = await _db.Houses.FirstOrDefaultAsync(u => u.Id == id);
             if (house == null)
             {
                 return NotFound();
             }
             _db.Houses.Remove(house);
-            _db.SaveChanges();
+            await _db.SaveChangesAsync();
             return NoContent();
         }
 
@@ -123,7 +123,7 @@ namespace MagicHouse_HouseAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         
 
-        public IActionResult UpdateHouse(int id, [FromBody] HouseUpdateDTO houseDTO)
+        public async Task<IActionResult> UpdateHouse(int id, [FromBody] HouseUpdateDTO houseDTO)
         {
             if (houseDTO == null || id != houseDTO.Id)
             {
@@ -145,8 +145,8 @@ namespace MagicHouse_HouseAPI.Controllers
                 Rate = houseDTO.Rate,
                 Sqft = houseDTO.Sqft,
             };
-            _db.Houses.Update(model);
-            _db.SaveChanges();
+             _db.Houses.Update(model);
+            await _db.SaveChangesAsync();
 
             return NoContent();
         }
@@ -157,13 +157,13 @@ namespace MagicHouse_HouseAPI.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
 
-        public IActionResult UpdatePartialHouse(int id,JsonPatchDocument<HouseUpdateDTO>patchDTO)
+        public async Task<IActionResult> UpdatePartialHouse(int id,JsonPatchDocument<HouseUpdateDTO>patchDTO)
         {
             if(patchDTO == null || id == 0)
             {
                 return BadRequest();
             }
-            var house = _db.Houses.AsNoTracking().FirstOrDefault(u => u.Id == id);
+            var house = await _db.Houses.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
 
 
             HouseUpdateDTO houseDTO = new ()
@@ -194,7 +194,7 @@ namespace MagicHouse_HouseAPI.Controllers
                 Sqft = houseDTO.Sqft,
             };
             _db.Houses.Update(model);
-            _db.SaveChanges();
+            await _db.SaveChangesAsync();
             if (!ModelState.IsValid) 
             {
                 return BadRequest();
